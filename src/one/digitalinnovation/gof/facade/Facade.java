601835c5c408f790d52ac7b865d821f50c5dea31
danie1souzasilva <119264364+danie1souzasilva@users.noreply.github.com>
2023-09-23 19:22:10 -0300

    Update Facade.java

--- src/one/digitalinnovation/gof/facade/Facade.java
+++ src/one/digitalinnovation/gof/facade/Facade.java
@@ -3,7 +3,7 @@ package one.digitalinnovation.gof.facade;
 import subsistema1.crm.CrmService;
 import subsistema2.cep.CepApi;
 
-public class Facade {
+public class FacadeD {
 
 	public void migrarCliente(String nome, String cep) {
 		String cidade = CepApi.getInstancia().recuperarCidade(cep);
